409972a1c0209e8aff8da4dc1f32c94156f40bf1
damodara <jay108@mail.ru>
2018-03-09 22:57:45 +0300

    create admin page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,48 @@
 @extends('admin.layouts.app_admin')
 
 @section('content')
-    <h1>admin page</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Categories 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Materials 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Visitors 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Today 0</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Create category</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">Category 1</h4>
+                    <p class="list-group-item-text">
+                        All Materials
+                    </p>
+                </a>
+            </div>
+            <div class="col-sm-6">
+                <a class="btn btn-block btn-default" href="#">Create material</a>
+                <a class="list-group-item" href="#">
+                    <h4 class="list-group-item-heading">Material 1</h4>
+                    <p class="list-group-item-text">
+                        Category
+                    </p>
+                </a>
+            </div>
+        </div>
+    </div>
 @endsection
